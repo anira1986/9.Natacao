629a7ad00e6feadbf3a209b1313ff87c610d3b4f
ANA PAULA SILVA <ana.osilva1986@gmail.com>
2024-10-25 21:36:23 -0300

    Update JuvenilNadadorFragment.java

--- app/src/main/java/br/edu/fateczl/natacao/JuvenilNadadorFragment.java
+++ app/src/main/java/br/edu/fateczl/natacao/JuvenilNadadorFragment.java
@@ -32,7 +32,7 @@ public class JuvenilNadadorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+      
         view = inflater.inflate(R.layout.fragment_juvenil, container, false);
         inputNomeJuvenil = view.findViewById(R.id.inputNomeJuvenil);
         inputDataNascimentoJuvenil = view.findViewById(R.id.inputDataNascimentoJuvenil);
